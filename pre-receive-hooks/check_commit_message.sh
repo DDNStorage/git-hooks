f66fafdb8c6a94e52b65a0ff971c708fb566ec42
Antoine Sax <asax@ddn.com>
2023-01-20 10:53:13 +0100

    add scope into title

--- pre-receive-hooks/check_commit_message.sh
+++ pre-receive-hooks/check_commit_message.sh
@@ -6,7 +6,21 @@
 set -e
 
 zero_commit='0000000000000000000000000000000000000000'
-msg_regex='^[A-Z]+\-[0-9]+: '
+# matching title with:
+# - JIRA ticket id (ex: RED-4322) followed by
+# - optional scope (ex: core, io, ci, controlplane), only alphabetic characters allowed. followed by
+# - a colon, followed by 1 space
+#
+# valid examples:
+# - RED-124: my title
+# - RED-456(core): my title
+#
+# invalid ones:
+# - title without JIRA issue
+# - RED-45:title, missing space
+# - RED-45(): missing scope in parenthesis
+
+msg_regex='^[A-Z]+\-[0-9]+(\([a-zA-Z]+\))?: '
 
 while read -r oldrev newrev refname; do
 
